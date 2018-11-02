2fa8cbf53e4a2e1ced5d300cf5051cecc6441f11
EvgenyPlotnikov45 <dlccc4596@yandex.ru>
2018-11-02 09:56:22 +0500

    trying getting lyrics

--- popup.js
+++ popup.js
@@ -9,15 +9,17 @@ document.addEventListener("DOMContentLoaded", function () {
         const q = input.value;
         request(`search?q=${encodeURIComponent(q)}`).then(result => {
             const lyricsContainer = document.getElementById("lyrics");
+            const songContainer = document.getElementById("song");
             let artistsHtml = "";
-            lyricsContainer.innerHTML = "";
             result.hits.forEach(item => {
                 const title = item.result.title;
                 const artistName = item.result.primary_artist.name;
                 const imgSrc = item.result.primary_artist.image_url;
+                const songId = item.result.id;
+                console.log(songId);
                 // language=HTML
                 artistsHtml += `
-                    <div class="songItem">
+                    <div class="songItem" data-id="${songId}">
                         <div class="artistImg">
                             <img src="${imgSrc}" alt="">
                         </div>
@@ -29,13 +31,33 @@ document.addEventListener("DOMContentLoaded", function () {
                 `;
             });
             lyricsContainer.innerHTML = artistsHtml;
-            console.log(result);
-        })
+            Array.prototype.forEach.call(lyricsContainer.getElementsByClassName("songItem"), item => {
+                item.addEventListener("click", event => {
+                    const id = event.currentTarget.getAttribute("data-id");
+                    console.log(id);
+                    request(`songs/${id}`).then(result => {
+                        // lyricsContainer.innerHTML = "";
+                        console.log(eval(result.song.embed_content));
+                        // songContainer.innerHTML = result.song.embed_content;
+                        // console.log(result);
+                    });
+                });
+            });
+        });
     });
 });
 
 function request(path) {
-    const url = `https://api.genius.com/${path}&access_token=${accessToken}`;
+    let url = `https://api.genius.com/${path}`;
+    let existQueryParams = false;
+    for (let i = 0; i < path.length; i++) {
+        if (path[i] === "?") existQueryParams = true;
+    }
+    if(existQueryParams) {
+        url += `&access_token=${accessToken}`;
+    } else {
+        url += `?access_token=${accessToken}`;
+    }
 
     return fetch(url).then(body => body.json()).then(result => {
         if (result.error)
